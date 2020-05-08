39ec666f6e8ff83ac00b3d04af8ee62c27a39a61
Kinolite <40676441+Kinolite@users.noreply.github.com>
2020-05-08 17:20:50 -0400

    Moved category from Developer to System

--- src/commands/System.js
+++ src/commands/System.js
@@ -2,7 +2,7 @@ const { CommandCategory } = require("../index.js");
 
 module.exports = class extends CommandCategory {
     constructor(name) {
-        super(name, "The Developer category is for commands only bot developers are allowed to run. These commands " +
+        super(name, "The System category is for commands only bot developers are allowed to run. These commands " +
         "hold the power to cause a mismatch exception in the universe, therefore creating a ripple through time and " +
         "space.");
     }
