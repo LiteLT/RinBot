85a6a0e6ad4debdafe765298a73d405ac9b0a27d
Kinolite <40676441+Kinolite@users.noreply.github.com>
2020-05-08 12:15:44 -0400

    Fixed Command + Subcommand path & added CommandCategory path

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ module.exports = {
     Logger: require("./structures/Logger.js"),
     Constants: require("./utils/Constants.js"),
     Endpoints: require("./utils/Endpoints.js"),
+    Command: require("./structures/Command.js"),
     Schedule: require("./structures/Schedule.js"),
     CommandError: require("./utils/CommandError.js"),
     Collection: require("./structures/Collection.js"),
-    Command: require("./structures/Command/Command.js"),
+    Subcommand: require("./structures/Subcommand.js"),
     HypixelLeveling: require("./utils/HypixelLeveling.js"),
     EventListener: require("./structures/EventListener.js"),
-    Subcommand: require("./structures/Command/Subcommand.js"),
+    CommandCategory: require("./structures/CommandCategory.js"),
     MessageCollector: require("./structures/MessageCollector.js"),
     ReactionCollector: require("./structures/ReactionCollector.js")
 };
