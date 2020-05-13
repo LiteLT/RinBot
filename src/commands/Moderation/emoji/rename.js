f1081c95292dc1bc458c201cd93f9f757a34077c
Kinolite <40676441+Kinolite@users.noreply.github.com>
2020-05-13 10:09:04 -0400

    Moved permission checks to single method call on base command class.

--- src/commands/Moderation/emoji/rename.js
+++ src/commands/Moderation/emoji/rename.js
@@ -10,21 +10,15 @@ module.exports = class extends Subcommand {
             requiredArgs: 2
         });
     }
-  
+
     async run(message, [emojiArg, ...name]) {
-        if (!message.channel.permissionsOf(message.author.id).has("manageEmojis")) {
-            return message.channel.createMessage("You do not have permission to run this command.\n\n" +
-            "Missing: `Manage Emojis`.");
-        } else if (!message.channel.permissionsOf(this.client.user.id).has("manageEmojis")) {
-            return message.channel.createMessage("I do not have permission to perform this action.\n\n" +
-            "Missing: `Manage Emojis`.");
+        let res = this.command.check(message, null);
+
+        if (!res) {
+            return;
         }
 
         name = name.join(" ");
-
-        if (name.length < 2 || name.length > 32) {
-            return CommandError.ERR_INVALID_RANGE(message, "name", "emoji name", 2, 32);
-        }
 
         let emoji = this.client.commands.get("emoji").findEmoji(message.channel.guild, emojiArg);
 
