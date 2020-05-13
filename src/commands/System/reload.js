f20079945eae19d56c6e37e66658543f61206d07
Kinolite <40676441+Kinolite@users.noreply.github.com>
2020-05-13 10:06:52 -0400

    Added support for categories with spaces

--- src/commands/System/reload.js
+++ src/commands/System/reload.js
@@ -14,44 +14,49 @@ module.exports = class extends Command {
             validatePermissions: (message) => Constants.BOT_DEVELOPERS.includes(message.author.id)
         });
     }
-  
-    async run(message, [arg, subcommand]) {
-        let search = arg.toLowerCase();
-      
+
+    /**
+     * Runs the command.
+     * @param {Eris.Message} message The message the command was called on.
+     * @param {Array<String>} args Arguments passed to the command.
+     */
+    async run(message, args) {
+        let search = args[0].toLowerCase();
+
         if (search === "all" || search === "*") {
             return this._reloadAll(message);
         }
-      
+
         let command = this.client.commands.get(this.client.aliases.get(search) || search);
-      
+        let subcommand = args[1]?.toLowerCase();
+
         if (command) {
             if (subcommand) {
-                let subcmd = command.subcommands.get(subcommand.toLowerCase());
-              
+                let subcmd = command.subcommands.get(subcommand);
+
                 if (subcmd) {
                     return this._reloadSubcommand(message, command, subcmd);
                 }
-              
-                return CommandError.ERR_NOT_FOUND(message, "subcommand of command " +
-                                                  `\`${command.name}\``, subcommand);
+
+                return CommandError.ERR_NOT_FOUND(message, `subcommand of command \`${command.name}\``, subcommand);
             }
-          
+
             return this._reloadCommand(message, command);
         }
-      
-        let categories = Util.arrayUnique(this.client.commands.map((command) => command.category.name));
-        let category = categories.find((category) => category.toLowerCase() === search);
-      
+
+        let category = Util.arrayUnique(this.client.commands.map((command) => command.category.name))
+            .find((category) => category === Util.toTitleCase(args.join(" ")));
+
         if (category) {
             return this._reloadCategory(message, category);
         }
-      
+
         return CommandError.ERR_NOT_FOUND(message, "command or category", arg);
     }
-  
+
     _reloadAll(message) {
         let failed = [];
-      
+
         for (const [, command] of this.client.commands) {
             try {
                 delete require.cache[require.resolve(`../${command.category.name}/${command.name}.js`)];
@@ -59,33 +64,34 @@ module.exports = class extends Command {
                 failed.push(command.name);
             }
         }
-      
+
         this.client.commands.clear();
         this.client.aliases.clear();
         this.client.globalRatelimit.clear();
-      
+
         this.client.init(["events", "connect"]).then(() => {
             return message.channel.createMessage(`${Constants.CustomEmojis
-                .CHECKMARK} Reloaded all comands.${failed.length
+                .CHECKMARK} Reloaded all commands.${failed.length
                 ? `\n\nFailed commands: \`${failed.join("`, `")}\``
                 : ""}`);
         });
     }
-  
+
     async _reloadCategory(message, categoryName) {
         let commands = [...this.client.commands.filter((command) => command.category.name === categoryName).values()];
         let category = commands[0].category;
-      
+
         for (const command of commands) {
             delete require.cache[require.resolve(`../${category.name}/${command.name}.js`)];
             this.client.commands.delete(command.name);
-          
+
             for (const alias of command.aliases) {
                 this.client.aliases.delete(alias);
             }
         }
-      
+
         const commandFiles = await fs.readdir(`src/commands/${categoryName}/`);
+
         for (const commandFile of commandFiles) {
             let command = new (require(`../${categoryName}/${commandFile}`))(this.client, commandFile, category);
 
@@ -99,33 +105,33 @@ module.exports = class extends Command {
         return message.channel.createMessage(`${Constants.CustomEmojis
             .CHECKMARK} Reloaded category \`${categoryName}\`.`);
     }
-  
+
     async _reloadCommand(message, command) {
         let hasSubcommands = command.subcommands.size;
-              
+
         delete require.cache[require.resolve(`../${command.category.name}/${command.name}.js`)];
         this.client.commands.delete(command.name);
-      
+
         for (const alias of command.aliases) {
             this.client.aliases.delete(alias);
         }
-      
+
         for (const [subcommand] of command.subcommands) {
             let pathway = `../${command.category.name}/${command.name}/${subcommand}.js`;
-          
+
             delete require.cache[require.resolve(pathway)];
         }
-      
+
         command.subcommands.clear();
         command = new (require(`../${command.category.name}/${command
             .name}.js`))(this.client, `${command.name}.js`, command.category);
-      
+
         this.client.commands.set(command.name, command);
-      
+
         for (const alias of command.aliases) {
             this.client.aliases.set(alias, command.name);
         }
-      
+
         if (hasSubcommands > 0) {
             let subcommands = await fs.readdir(`src/commands/${command.category.name}/${command.name}/`);
 
@@ -136,20 +142,20 @@ module.exports = class extends Command {
                 command.subcommands.set(subcommand.name, subcommand);
             }
         }
-      
+
         return message.channel.createMessage(`${Constants.CustomEmojis
             .CHECKMARK} Reloaded command \`${command.name}\`.`);
     }
-  
+
     _reloadSubcommand(message, command, subcommand) {
         let pathway = `../${command.category.name}/${command.name}/${subcommand.name}.js`;
-      
+
         delete require.cache[require.resolve(pathway)];
         command.subcommands.delete(subcommand.name);
-      
+
         subcommand = new (require(pathway))(this.client, command, `${subcommand.name}.js`);
         command.subcommands.set(subcommand.name, subcommand);
-      
+
         return message.channel.createMessage(`${Constants.CustomEmojis
             .CHECKMARK} Reloaded subcommand \`${subcommand.name}\`.`);
     }
