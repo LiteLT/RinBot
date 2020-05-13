b912db7fcc20bd070bb96dff3b5de6ef443128ab
Kinolite <40676441+Kinolite@users.noreply.github.com>
2020-05-13 09:58:11 -0400

    Added JSDoc

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -6,12 +6,15 @@ module.exports = class extends EventListener {
     constructor(...args) {
         super(...args, { once: true });
     }
-  
+
+    /**
+     * Runs the event listener.
+     */
     async run() {
         let botTag = Util.userTag(this.client.user);
         let guildCount = this.client.guilds.size;
         let userCount = this.client.guilds.reduce((prev, guild) => prev + guild.memberCount, 0);
-      
+
         this.client.logger.log(`Authenticated as ${botTag} (${this.client.user
             .id}) | Serving ${guildCount} guilds and ${userCount} users.`);
 
