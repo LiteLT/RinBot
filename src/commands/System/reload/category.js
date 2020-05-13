e66aede8958df6036a9102c9b25757ab77669693
Kinolite <40676441+Kinolite@users.noreply.github.com>
2020-05-13 02:16:15 -0400

    Added support for categories with names

--- src/commands/System/reload/category.js
+++ src/commands/System/reload/category.js
@@ -5,23 +5,22 @@ const { Util, Subcommand, CommandError } = require("../../../index.js");
 module.exports = class extends Subcommand {
     constructor(...args) {
         super(...args, {
+            usage: "<category>",
             description: "Reload a category.",
-            fullDescription: "This is useful when both a command and category share the same " +
-            "name.",
+            fullDescription: "This is useful when both a command and category share the same name.",
             requiredArgs: 1,
             enabled: true
         });
     }
-  
-    async run(message, [arg]) {
-        let search = arg.toLowerCase();
-        let categories = Util.arrayUnique(this.client.commands.map((command) => command.category.name));
-        let category = categories.find((category) => category.toLowerCase() === search);
-      
+
+    async run(message, args) {
+        let category = Util.arrayUnique(this.client.commands.map((command) => command.category.name))
+            .find((category) => category === Util.toTitleCase(args.join(" ")));
+
         if (category) {
             return this.command._reloadCategory(message, category);
         }
-      
-        return CommandError.ERR_NOT_FOUND(message, "category", arg);
+
+        return CommandError.ERR_NOT_FOUND(message, "category", args.join(" "));
     }
 };
