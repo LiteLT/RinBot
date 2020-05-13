8c78c38235e53fd1435be59ee71a9f61746c90cc
Kinolite <40676441+Kinolite@users.noreply.github.com>
2020-05-13 10:09:27 -0400

    Moved color codes transformation to single method call on base command class.

--- src/commands/Tools/colorinfo/rgb.js
+++ src/commands/Tools/colorinfo/rgb.js
@@ -10,23 +10,25 @@ module.exports = class extends Subcommand {
             requiredArgs: 1
         });
     }
-  
+
     async run(message, args) {
         let command = this.client.commands.get("colorinfo");
         let colorInput = args.join(" ");
         let color = args.join(" ").match(/^(?:rgb)?\(? *(\d{1,3}) *, *(\d{1,3}) *, *(\d{1,3}) *\)?$/i);
 
         if (color) {
-            let colorCodes = color.slice(1);
-            colorCodes.forEach((color, index) => {
+            let colorCodes = color.slice(1).map((color) => {
                 let colorNum = parseInt(color, 10);
 
                 if (colorNum > 255) {
-                    colorCodes[index] = "255";
+                    return "255";
                 } else if (colorNum < 0) {
-                    colorCodes[index] = "0";
+                    return "0";
                 }
+
+                return color;
             });
+
             let urlColor = `rgb(${colorCodes.join(",")})`;
 
             return command._request(this.name, urlColor).then((colorData) => command.result(message, colorData));
