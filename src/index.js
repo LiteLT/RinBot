dee0d9fc3c22a19559adb52916d5b077f0ad928a
Kinolite <40676441+Kinolite@users.noreply.github.com>
2020-05-13 02:37:11 -0400

    Updated path to Rin.js file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 module.exports = {
-    Client: require("./Client.js"),
+    Client: require("./Rin.js"),
     Util: require("./utils/Util.js"),
     Logger: require("./structures/Logger.js"),
     Constants: require("./utils/Constants.js"),
     Endpoints: require("./utils/Endpoints.js"),
     Command: require("./structures/Command.js"),
-    Schedule: require("./structures/Schedule.js"),
     CommandError: require("./utils/CommandError.js"),
     Collection: require("./structures/Collection.js"),
     Subcommand: require("./structures/Subcommand.js"),
