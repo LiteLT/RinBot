6e185ac843b4fbecc206784051c634a9eec1bbd8
Kinolite <40676441+Kinolite@users.noreply.github.com>
2020-05-13 02:30:28 -0400

    Updated wording & removed singular variable use.

--- src/commands/System/eval.js
+++ src/commands/System/eval.js
@@ -2,6 +2,7 @@
 
 const { Util, Command, Constants } = require("../../index.js");
 const { inspect } = require("util");
+// noinspection JSUnusedLocalSymbols
 const fetch = require("node-fetch"); // eslint-disable-line no-unused-vars
 
 module.exports = class extends Command {
@@ -9,9 +10,6 @@ module.exports = class extends Command {
         super(...args, {
             usage: "<code>",
             description: "Evaluates arbitrary JavaScript.",
-            fullDescription: "The eval command evaluates code as-in, any error thrown from it " +
-            "will be handled. If the output is too large, a file will be sent instead. If the " +
-            "bot lacks permission to `Attach Files`, it will be sent to the console instead.",
             requiredArgs: 1,
             protected: true,
             validatePermissions: (message) => Constants.BOT_DEVELOPERS.includes(message.author.id),
@@ -19,40 +17,37 @@ module.exports = class extends Command {
                 {
                     name: "depth",
                     value: "number",
-                    description: "Sets the depth limit for the object. As the limit increases, " +
-                    "the result size becomes larger. Avoid specifying a limit if you know it'll " +
-                    "exceed the 2000 character limit."
+                    description: "Sets the depth limit for the value returned from the eval expression."
                 },
                 {
                     name: "async",
-                    description: "Wraps the expression in an async function. This allows for the " +
-                    "use of the `await` keyword. As a downside, you must use the `return` " +
-                    "keyword to display the result."
+                    description: "Wraps the expression in an async function. This allows for the use of the `await` " +
+                    "keyword. As a downside, you must use the `return` keyword to retrieve the result."
                 },
                 {
                     name: "silent",
-                    description: "Makes the bot not send a message after evalaution, regardless " +
-                    "of if an exception was thrown."
+                    description: "Makes the bot not send a message after evaluation, regardless of if an exception " +
+                    "was thrown."
                 },
                 {
-                    name: "showHidden",
-                    description: "Displays hidden properties within the returned object."
+                    name: "showhidden",
+                    description: "Exposes hidden properties within the returned object."
                 }
             ]
         });
     }
-  
+
     async run(message, args) {
         let input = args.join(" ").replace(/^`{3}\w+\n|`{3}$/g, "");
         let flags = Util.messageFlags(message, this.client);
-      
+
         if (flags.async) {
             input = `(async () => { ${input} })()`;
         }
-      
+
         try {
             let result = await eval(input); // eslint-disable-line no-eval
-          
+
             if (!flags.silent) {
                 if (typeof result !== "string") {
                     result = inspect(result, {
@@ -61,18 +56,18 @@ module.exports = class extends Command {
                         showHidden: !!flags.showHidden
                     });
                 }
-              
-                result = result.replace(new RegExp(Constants
-                    .DISCORD_TOKEN, "g"), "--- Deducted ---");
-                let wrap = `\`\`\`js\n${result}\`\`\``;
-              
+
+                result = result.replace(new RegExp(Constants.DISCORD_TOKEN, "g"), "--- Deducted ---");
+
+                // It returns the content faster if you suppress it.
+                // noinspection ES6MissingAwait
                 message.addReaction(Constants.Emojis.BLUE_CHECK);
-              
-                return await message.channel.createMessage(wrap);
+
+                return await message.channel.createMessage(`\`\`\`js\n${result}\`\`\``);
             }
         } catch (ex) {
             if (!flags.silent) {
-                message.channel.createMessage("```js\n" + ex + "```").catch(() => {});
+                return message.channel.createMessage(`\`\`\`js\n${ex}\`\`\``).catch(() => {});
             }
         }
     }
