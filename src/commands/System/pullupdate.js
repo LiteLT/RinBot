a0ec76769de613d6466406c582d32f8ae7825933
Lite <40676441+Kinolite@users.noreply.github.com>
2020-06-17 22:11:11 -0400

    Create pullupdate.js

---
+++ src/commands/System/pullupdate.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const { Command } = require("../../index.js");
+const { execCommand } = require("child_process");
+const { promisify } = require("util");
+const exec = promisify(execCommand);
+
+module.exports = class extends Command {
+    constructor(...args) {
+        super(...args, {
+            description: "Downloads the latest version of the bot.",
+            protected: true,
+            validatePermissions: (message) => Constants.BOT_DEVELOPERS.includes(message.author.id)
+        });
+    }
+
+    /**
+     * Runs the command.
+     *
+     * @param {Eris.Message} message The message the command was called on.
+     * @param {Array<String>} args Arguments passed to the command.
+     */
+    async run(message, args) {
+        await message.channel.createMessage("Updating...");
+
+        try {
+            await exec("sudo git pull --no-commit origin && pm2 restart rin");
+            await this.client.gracefulExit(0); // Just to be safe.
+        } catch (ex) {
+            return message.channel.createMessage(`Failed to download update: ${ex.stack || ex}`);
+        }
+    }
+};
