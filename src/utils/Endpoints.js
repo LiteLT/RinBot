baf706b19239b0b804e25e78e3f58b75d8134ddf
Lite <40676441+Kinolite@users.noreply.github.com>
2020-06-19 18:05:52 -0400

    Added Hypixel forums endpoint

--- src/utils/Endpoints.js
+++ src/utils/Endpoints.js
@@ -8,6 +8,7 @@ const PLANCKE_URL = "https://plancke.io/hypixel";
 const DUMMY_IMAGE_URL = "https://dummyimage.com";
 const MOJANG_API_URL = "https://api.mojang.com";
 const ANILIST_URL = "https://anilist.co";
+const HYPIXEL_FORUMS_URL = "https://hypixel.net";
 
 module.exports = {
     ANILIST_GRAPHQL: "https://graphql.anilist.co/",
@@ -17,7 +18,7 @@ module.exports = {
     DAILYMOTION_VIDEO: (videoID) => `https://dailymotion.com/video/${videoID}`,
 
     DISCORD_EMOJI_ICON_URL: (emojiID, type = "png") => `${DISCORD_CDN_URL}/emojis/${emojiID}.${type}?v=1`,
-    
+
     DUMMY_IMAGE_IMAGE: (hex, name) => `${DUMMY_IMAGE_URL}/300x300/${hex}.png&text=${name}`,
 
     HYPIXEL_API_KEY: (key) => `${HYPIXEL_API_URL}/key?key=${key}`,
@@ -25,6 +26,7 @@ module.exports = {
     HYPIXEL_API_GUILD_GUILD: (key, guild) => `${HYPIXEL_API_URL}/guild?name=${guild}&key=${key}`,
     HYPIXEL_API_PLAYER_UUID: (key, uuid) => `${HYPIXEL_API_URL}/player?uuid=${uuid}&key=${key}`,
     HYPIXEL_API_PLAYER_USERNAME: (key, name) => `${HYPIXEL_API_URL}/player?name=${name}&key=${key}`,
+    HYPIXEL_FORUMS_PROFILE: (forumID) => `${HYPIXEL_FORUMS_URL}/members/${forumID}/`,
 
     MINECRAFT_UUID_NAMEHISTORY: (uuid) => `${MOJANG_API_URL}/user/profiles/${uuid}/names`,
 
@@ -39,7 +41,7 @@ module.exports = {
     SLOTHPIXEL_GUILD_PLAYERS: (username) => `${SLOTHPIXEL_URL}/guilds/${username}?populatePlayers`,
 
     THE_COLOR_API_ID: (type, value) => `${THE_COLOR_API_URL}/id?${type}=${value}&format=json`,
-    
+
     VISAGE_BUST: (uuid, size = 600) => `${VISAGE_URL}/bust/${size}/${uuid}`,
 
     YOUTUBE_VIDEO: (videoID) => `https://youtube.com/watch?v=${videoID}`
